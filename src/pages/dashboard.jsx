df43ec63d6634d07d7e3841763076d118ef09437
YankhairunisaZahra <111202214328@mhs.dinus.ac.id>
2024-12-08 21:18:03 +0700

    update error

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,16 +4,12 @@ import CardBill from "../components/Fragments/dashboard/CardBill";
 import CardExpenseBreakdown from "../components/Fragments/dashboard/CardExpenseBreakdown";
 import CardTransaction from "../components/Fragments/dashboard/CardTransaction";
 import CardBalance from "../components/Fragments/dashboard/CardBalance";
-import CardStatistic from "../components/Fragments/dashboard/CardStatistic";
-import CardGoal from "../components/Fragments/dashboard/CardGoal";
-
 const DashboardPage = () => {
   return (
     <MainLayout type="dashboard">
       {/* top content start*/}
       <div className="md:grid md:grid-cols-3 md:gap-x-6">
         <CardBalance />
-        <CardGoal />
         <CardBill />
         <CardTransaction />
         <CardStatistic />
